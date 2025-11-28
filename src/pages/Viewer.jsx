8e0d35bc28417901a21ba246317c3e54f67c60b7
Nobuo Kato <katonobu4649@gmail.com>
2025-11-28 06:51:33 +0000

    別ページで開く画像リンクにオープンアイコンを追加

--- src/pages/Viewer.jsx
+++ src/pages/Viewer.jsx
@@ -61,7 +61,18 @@ export default function Viewer() {
         metadata.files.map(item => (
           <>
           <p key={`p-${item.id}`}><a href="#top" className="text-blue-500 hover:underline">ページの先頭</a></p>
-          <a key={`a-${item.id}`} href={`/${dir}/${item.name}`} target="_blank" id={item.id} className="text-blue-500 hover:underline">{item.title}</a>
+          <a 
+            key={`a-${item.id}`}
+            href={`/${dir}/${item.name}`}
+            target="_blank"
+            id={item.id}
+            className="text-blue-500 hover:underline inline-flex items-center gap-1"
+          >
+            {item.title}
+            <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true">
+              <path d="M3.75 2h3.5a.75.75 0 0 1 0 1.5h-3.5a.25.25 0 0 0-.25.25v8.5c0 .138.112.25.25.25h8.5a.25.25 0 0 0 .25-.25v-3.5a.75.75 0 0 1 1.5 0v3.5A1.75 1.75 0 0 1 12.25 14h-8.5A1.75 1.75 0 0 1 2 12.25v-8.5C2 2.784 2.784 2 3.75 2Zm6.854-1h4.146a.25.25 0 0 1 .25.25v4.146a.25.25 0 0 1-.427.177L13.03 4.03 9.28 7.78a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042l3.75-3.75-1.543-1.543A.25.25 0 0 1 10.604 1Z"></path>
+            </svg>
+          </a>
           <img key={`img-${item.id}`} width="100%" height="auto" src={`/${dir}/${item.name}`} className="w-full h-auto border-2 border-black"></img>
           </>
         ))
