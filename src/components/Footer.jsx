59f936cb7028a517fa6187f39bc766df82c999be
Nobuo Kato <katonobu4649@gmail.com>
2025-12-04 23:53:12 +0900

    dev,stg環境ではfooterに環境名を表示させるようにした。

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,29 +2,33 @@ import { Link } from '@tanstack/react-router'
 import { isTankiYohou } from '../hooks/useFetchKaisetsuData'
 import { TextToSpeechCtrl } from '../components/TextToSpeechCtrl'
 
-export const Footer = ({ metadata, kaisetsu, kaisetsuLoading }) => (
-  <>
-    <hr></hr>
-    {isTankiYohou(metadata) ? <TextToSpeechCtrl texts={kaisetsu} loading={kaisetsuLoading}></TextToSpeechCtrl> : null}
-    <hr></hr>
-    <p className="text-sm">
-      <a href="https://www.jma.go.jp/jma/index.html" className="text-blue-500 hover:underline">
-      気象庁ホームページ
-      </a>のデータを加工して作成。
-      引用元URL、加工内容の詳細は、
+export const Footer = ({ metadata, kaisetsu, kaisetsuLoading }) => {
+  const envTarget = import.meta.env.VITE_ENV;
+  const envTargetStr = envTarget==="prd"?"":(envTarget==="stg"?"ステージング環境用":"開発環境用")
+  return (
+    <>
+      <hr></hr>
+      {isTankiYohou(metadata) ? <TextToSpeechCtrl texts={kaisetsu} loading={kaisetsuLoading}></TextToSpeechCtrl> : null}
+      <hr></hr>
+      <p className="text-sm">
+        <a href="https://www.jma.go.jp/jma/index.html" className="text-blue-500 hover:underline">
+        気象庁ホームページ
+        </a>のデータを加工して作成。
+        引用元URL、加工内容の詳細は、
+        <Link
+          to={`/reference`}
+          className="text-sm text-blue-500 hover:underline"
+        >
+          こちら
+        </Link>
+      </p>
       <Link
-        to={`/reference`}
+        to={`/about`}
         className="text-sm text-blue-500 hover:underline"
       >
-        こちら
+        このページについて
       </Link>
-    </p>
-    <Link
-      to={`/about`}
-      className="text-sm text-blue-500 hover:underline"
-    >
-      このページについて
-    </Link>
-    <p className="text-xs">© {new Date().getFullYear()} katonobu. All rights reserved. Build at {import.meta.env.VITE_BUILD_TIME}</p>
-  </>
-)
+      <p className="text-xs">© {new Date().getFullYear()} katonobu. All rights reserved. Build at {import.meta.env.VITE_BUILD_TIME} {envTargetStr}</p>
+    </>
+  )
+}
