09a12043b5f1aadb4343b47b09a68a7b53cf145e
Nobuo Kato <katonobu4649@gmail.com>
2025-12-05 00:22:40 +0900

    参照していた環境変数間違えていたのを修正

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   const env = mode // ここでは mode をそのまま利用
 
   return {
-    base: process.env.VITE_BASE || '/',
+    base: process.env.VITE_BASE_PATH || '/',
     plugins: [
       react(),
       tailwindcss(),
