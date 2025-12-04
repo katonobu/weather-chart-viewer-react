dc0451b4b654b9222e12cedfeb6d64bf182e0e9b
Nobuo Kato <katonobu4649@gmail.com>
2025-12-04 23:54:03 +0900

    ビルド時に環境に応じたrobots.txtの名前をrobots.txtに変えてコピー

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+export default defineConfig(({ mode }) => {
+  // mode は build 時に --mode dev/stg/prd で渡される
+  const env = mode // ここでは mode をそのまま利用
+
+  return {
+    base: process.env.VITE_BASE || '/',
+    plugins: [
+      react(),
+      tailwindcss(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: `public/robots.${env}.txt`, // 環境ごとの robots ファイル
+            dest: '.',                       // dist/ の直下にコピー
+            rename: 'robots.txt'             // ファイル名を robots.txt に固定
+          }
+        ]
+      })
+    ],
+  }
 })
+
